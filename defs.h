f560a7a2478c66331a4a994721ba10d7f2019aab
Eugene Syromiatnikov <esyr@redhat.com>
2017-08-01 17:12:40 +0200

    Introduce next_setqual_scno (needed for gdbserver backend)
    During initialisation, gdbserver backend enumerates all the syscall
    numbers enabled for tracing and asks GDB server to stop only on the
    specified syscall numbers. Since number_set interface is now opaque,
    we provide an ability to enumerate all the syscall numbers that are
    enabled.
    
    * defs.h (next_set_qual_scno): New declaration.
    * number_set.h (next_set_bit_in_set_array): Likewise.
    * filter_qualify.c (next_set_qual_scno): Implement it.
    * number_set.c (next_set_bit_in_set_array): Likewise.

--- defs.h
+++ defs.h
@@ -649,6 +649,8 @@ print_struct_statfs64(struct tcb *, kernel_ulong_t addr, kernel_ulong_t size);
 
 extern void print_ifindex(unsigned int);
 
+extern unsigned int next_set_qual_scno(const unsigned int scno,
+				       unsigned qual_flg);
 extern void qualify(const char *);
 extern unsigned int qual_flags(const unsigned int);
 
