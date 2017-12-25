2f63214fe5131e2f95be0a38c108fffd683ff0ee
Eugene Syromiatnikov <esyr@redhat.com>
2017-12-26 00:15:33 +0100

    syscall.c: wrap arch_set_{scno,error_success} in tracing-backend-specific wrapper

--- syscall.c
+++ syscall.c
@@ -556,7 +556,7 @@ tamper_with_syscall_entering(struct tcb *tcp, unsigned int *signo)
 
 	if (opts->data.flags & INJECT_F_SIGNAL)
 		*signo = opts->data.signo;
-	if (opts->data.flags & INJECT_F_RETVAL && !arch_set_scno(tcp, -1))
+	if (opts->data.flags & INJECT_F_RETVAL && !set_scno(tcp, -1))
 		tcp->flags |= TCB_TAMPERED;
 
 	return 0;
@@ -574,7 +574,7 @@ tamper_with_syscall_exiting(struct tcb *tcp)
 		kernel_long_t u_rval = tcp->u_rval;
 
 		tcp->u_rval = opts->data.rval;
-		if (arch_set_success(tcp)) {
+		if (set_success(tcp)) {
 			tcp->u_rval = u_rval;
 		} else {
 			tcp->u_error = 0;
@@ -586,7 +586,7 @@ tamper_with_syscall_exiting(struct tcb *tcp)
 			unsigned long u_error = tcp->u_error;
 
 			tcp->u_error = new_error;
-			if (arch_set_error(tcp)) {
+			if (set_error(tcp)) {
 				tcp->u_error = u_error;
 			}
 		}
@@ -1213,6 +1213,25 @@ get_scno(struct tcb *tcp)
 	}
 	return 1;
 }
+
+int
+set_scno(struct tcb *tcp, kernel_ulong_t scno)
+{
+	return arch_set_scno(tcp, scno);
+}
+
+int
+set_error(struct tcb *tcp)
+{
+	return arch_set_error(tcp);
+}
+
+int
+set_success(struct tcb *tcp)
+{
+	return arch_set_success(tcp);
+}
+
 
 #ifdef ptrace_getregset_or_getregs
 # define get_syscall_result_regs get_regs
