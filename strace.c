9a4559b111e5c403919f2022cd84cb12630424a2
Eugene Syromiatnikov <esyr@redhat.com>
2017-12-25 23:59:36 +0100

    strace.c: move ptrace backend initialisation into a separate routine
    In a preparation to TraBAL introduction.
    
    * strace.c (tracing_backend_init): New function.
    (tracing_backend_name): New macro.
    (init): Move OS release detection, ptrace_setoptions initialisation,
    and PTRACE_SEIZE test to tracing_backend_init.

--- strace.c
+++ strace.c
@@ -1652,6 +1652,25 @@ set_sighandler(int signo, void (*sighandler)(int), struct sigaction *oldact)
 	sigaction(signo, &sa, oldact);
 }
 
+bool
+tracing_backend_init(int argc, char *argv[])
+{
+	os_release = get_os_release();
+
+	if (followfork)
+		ptrace_setoptions |= PTRACE_O_TRACECLONE |
+				     PTRACE_O_TRACEFORK |
+				     PTRACE_O_TRACEVFORK;
+	debug_msg("ptrace_setoptions = %#x", ptrace_setoptions);
+
+
+	test_ptrace_seize();
+
+	return true;
+}
+
+#define tracing_backend_name() "ptrace"
+
 /*
  * Initialization part of main() was eating much stack (~0.5k),
  * which was unused after init.
@@ -1685,8 +1704,6 @@ init(int argc, char *argv[])
 	}
 
 	strace_tracer_pid = getpid();
-
-	os_release = get_os_release();
 
 	shared_log = stderr;
 	set_sortby(DEFAULT_SORTBY);
@@ -1883,6 +1900,10 @@ init(int argc, char *argv[])
 		tflag = 1;
 	}
 
+	if (!tracing_backend_init(argc, argv))
+		error_msg_and_die("Cannot initialize backend \"%s\".",
+				  tracing_backend_name());
+
 	for (cnt = 0; cnt < pathtrace_count; cnt++)
 		pathtrace_select(pathtrace_paths[cnt]);
 	free(pathtrace_paths);
@@ -1924,13 +1945,6 @@ init(int argc, char *argv[])
 		run_uid = getuid();
 		run_gid = getgid();
 	}
-
-	if (followfork)
-		ptrace_setoptions |= PTRACE_O_TRACECLONE |
-				     PTRACE_O_TRACEFORK |
-				     PTRACE_O_TRACEVFORK;
-	debug_msg("ptrace_setoptions = %#x", ptrace_setoptions);
-	test_ptrace_seize();
 
 	/*
 	 * Is something weird with our stdin and/or stdout -
